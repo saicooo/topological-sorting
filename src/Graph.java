3d9dc7e0ff096cc7460a8aa31640b20cbe59e65a
stepanviolet <stepanviolet@gmail.com>
2025-06-27 02:54:55 +0300

    Graph: add exists

--- src/Graph.java
+++ src/Graph.java
@@ -4,23 +4,35 @@ public class Graph {
     private List<Vertex> vertices;
 
     public Graph() {
-        this.vertices = new ArrayList<>();
+        this(new ArrayList<>());
+    }
+
+    public Graph(List<Vertex> vertices) {
+        this.vertices = vertices;
     }
 
     // добавление вершин
     public void addVertex (Vertex vertex) {
+        if (exists(vertex)) {
+            throw new IllegalArgumentException("Вершина в графе уже существует");
+        }
+
         vertices.add(vertex);
     }
 
     // добавление ребра
     public void addEdge(Vertex _s, Vertex _e) {
+        if (!exists(_s) || !exists(_e)) {
+            throw new IllegalArgumentException("Вершины добавляемых рёбер должны существовать в графе");
+        }
+        
         _s.addNeighbor(_e);
     }
 
-    //получаем вершину по имени
+    // получаем вершину по имени
     public Vertex getVertexByName (String name) {
         for (Vertex v : vertices) {
-            if (v.name.equals(name)) {
+            if (v.getName().equals(name)) {
                 return v;
             }
         }
@@ -30,6 +42,16 @@ public class Graph {
     // список вершин
     public List<Vertex> getVertices() {
         return vertices;
+    }
+
+    // проверка на существование вершины в графе
+    public boolean exists(Vertex vertex) {
+        for (Vertex v : vertices) {
+            if (vertex.equals(v)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     // вывод
@@ -42,5 +64,5 @@ public class Graph {
         return sb.toString();
     }
 
-
 }
+
