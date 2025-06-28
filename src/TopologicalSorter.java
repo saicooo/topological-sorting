057d43d1c3b8fe3931251d33facd8f962bd39275
bermaxim <maksb@LAPTOP-2HD1OHER.localdomain>
2025-06-28 03:21:51 +0300

    update TopSort

--- src/TopologicalSorter.java
+++ src/TopologicalSorter.java
@@ -1,54 +1,65 @@
 import java.util.*;
 
-public class TopologicalSorter {
+public class TopologicalSorter implements Iterator<Vertex> {
 
-    public static List<Vertex> sort(Graph graph) {
+    private final Graph graph;
+    private final Map<Vertex, Integer> inDegree;
+    private final Deque<Vertex> queue;
+    private final List<Vertex> result;
 
-        // накапливает итоговый
-        List<Vertex> result = new ArrayList<>();
-        
-        // карта входных степеней
-        Map<Vertex, Integer> inDegree = new HashMap<>();
+    public TopologicalSorter(Graph graph) {
+        this.graph = graph;
+        this.inDegree = new HashMap<>();
+        this.queue = new ArrayDeque<>();
+        this.result = new ArrayList<>();
 
-        // inDegree[v] = 0 
         for (Vertex v : graph.getVertices()) {
             inDegree.put(v, 0);
         }
 
-        // v->u: inDegree[u]++
         for (Vertex v : graph.getVertices()) {
             for (Vertex u : v.getNeighbors()) {
                 inDegree.put(u, inDegree.get(u) + 1);
             }
         }
 
-        Deque<Vertex> queue = new ArrayDeque<>();
-
-        // все вершины с inDegree = 0, в дек
         for (Map.Entry<Vertex, Integer> entry : inDegree.entrySet()) {
             if (entry.getValue() == 0) {
                 queue.addLast(entry.getKey());
             }
         }
 
-        
-        while (!queue.isEmpty()) {
-            Vertex v = queue.removeFirst();
-            result.add(v);
+    }
 
-            // для каждого соседа удаляем ребро v->neigh
-            for (Vertex neigh : v.getNeighbors()) {
-                int deg = inDegree.get(neigh) - 1;
-                inDegree.put(neigh, deg);
+    @Override
+    public boolean hasNext() {
+        return !queue.isEmpty();
+    }
 
-                if (deg == 0){
-                    queue.addLast(neigh);
-                }
+    @Override
+    public Vertex next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return iterateStep();
+    }
+
+    private Vertex iterateStep() {
+        Vertex v = queue.removeFirst();
+        result.add(v);
+
+        for (Vertex neigh : v.getNeighbors()) {
+            int deg = inDegree.get(neigh) - 1;
+            inDegree.put(neigh, deg);
+            if (deg == 0) {
+                queue.addLast(neigh);
             }
         }
+        return v;
+    }
 
-        return result;
-
+    public List<Vertex> getSortedSoFar() {
+        return Collections.unmodifiableList(result);
     }
 
     
