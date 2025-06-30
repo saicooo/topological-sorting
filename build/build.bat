aa01b54eb98f569552e81c898851bc35e80dd79e
Stepan Ivanov <stepanviolet@gmail.com>
2025-06-30 21:52:20 +0300

    setup: добавлены проверки наличия утилит и виртуальное окружение venv

--- build/build.bat
+++ build/build.bat
@@ -3,12 +3,57 @@ chcp 65001
 
 @REM Скрипт для сборки проекта на Windows
 
+echo Проверка наличия Python...
+where python >nul 2>nul
+set /a EXITCODE = %ERRORLEVEL%
+
+if %EXITCODE% neq 0 (
+    echo Python не найден. Пожалуйста, установите Python и добавьте его в PATH.
+    pause
+    exit %EXITCODE%
+)
+
+echo Проверка наличия pip...
+where pip >nul 2>nul
+set /a EXITCODE = %ERRORLEVEL%
+
+if %EXITCODE% neq 0 (
+    echo pip не найден. Пожалуйста, установите pip и добавьте его в PATH.
+    pause
+    exit %EXITCODE%
+)
+
+echo Проверка наличия venv...
+where python -m venv >nul 2>nul
+set /a EXITCODE = %ERRORLEVEL%
+
+if %EXITCODE% neq 0 (
+    echo Модуль venv не установлен. Установка venv...
+    python -m pip install virtualenv
+    set /a EXITCODE = %ERRORLEVEL%
+
+    if %EXITCODE% neq 0 (
+        echo Ошибка при установке venv. Пожалуйста, установите модуль venv вручную.
+        pause
+        exit %EXITCODE%
+    )
+)
+
+echo Создание виртуального окружения...
+python -m venv .venv
+call .venv\Scripts\activate.bat
+
+if %ERRORLEVEL% neq 0 (
+    echo Ошибка при активации виртуального окружения.
+    pause
+)
+
 echo Установка зависимостей из requirements.txt...
 pip install -r requirements.txt
 set /a EXITCODE = %ERRORLEVEL%
 
 if %EXITCODE% neq 0 (
-    echo Ошибка при установке зависимостей...
+    echo Ошибка при установке зависимостей.
     pause
     exit %EXITCODE%
 )
@@ -18,7 +63,7 @@ python -u .\build\install_jfx.py
 set /a EXITCODE = %ERRORLEVEL%
 
 if %EXITCODE% neq 0 (
-    echo Ошибка при установке JavaFX...
+    echo Ошибка при установке JavaFX.
     pause
     exit %EXITCODE%
 )
@@ -28,12 +73,17 @@ python -u .\build\build.py
 set /a EXITCODE = %ERRORLEVEL%
 
 if %EXITCODE% neq 0 (
-    echo Ошибка при сборке проекта...
+    echo Ошибка при сборке проекта.
     pause
     exit %EXITCODE%
 )
 
 echo Сборка проекта успешно завершена...
+
+echo Деактивация виртуального окружения...
+call .\.venv\Scripts\deactivate.bat
+rmdir /s /q .venv
+
 pause
 exit 0
 
--- setup.sh
+++ setup.sh
@@ -4,6 +4,41 @@
 
 declare -i EXITCODE
 
+echo "Проверка наличия Python 3..."
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 не установлен. Пожалуйста, установите Python 3."
+    exit 1
+fi
+
+echo "Проверка наличия pip..."
+if ! command -v pip3 &> /dev/null; then
+    echo "pip не установлен. Пожалуйста, установите pip."
+    exit 1
+fi
+
+echo "Проверка наличия venv..."
+if ! python3 -m venv --help &> /dev/null; then
+    echo "Модуль venv не установлен. Установка..."
+    pip3 install --break-system-packages virtualenv
+    EXITCODE=$?
+
+    if [ $EXITCODE -ne 0 ]; then
+        echo "Ошибка при установке venv. Пожалуйста, установите модуль venv вручную."
+        exit $EXITCODE
+    fi
+
+fi
+
+echo "Создание виртуального окружения..."
+python3 -m venv .venv
+source .venv/bin/activate
+EXITCODE=$?
+
+if [ $EXITCODE -ne 0 ]; then
+    echo "Ошибка при активации виртуального окружения."
+    read -p "Нажмите Enter для продолжения..."
+fi
+
 echo "Установка зависимостей из requirements.txt..."
 pip3 install -r requirements.txt
 EXITCODE=$?
@@ -32,4 +67,8 @@ if [ $EXITCODE -ne 0 ]; then
 fi
 
 echo "Сборка проекта успешно завершена..."
+
+echo "Деактивация виртуального окружения..."
+source .venv/bin/deactivate
+rm -r .venv
 
