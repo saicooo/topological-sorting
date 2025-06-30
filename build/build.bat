7ee68ff177fd4704b5adcfa54067779db2f770a3
Stepan Ivanov <stepanviolet@gmail.com>
2025-06-30 18:44:32 +0300

    setup: initegrate install_jfx.py into build.bat

--- build/build.bat
+++ build/build.bat
@@ -5,18 +5,28 @@ chcp 65001
 
 echo Установка зависимостей из requirements.txt...
 pip install -r requirements.txt
-
 set /a EXITCODE = %ERRORLEVEL%
+
 if %EXITCODE% neq 0 (
     echo Ошибка при установке зависимостей...
     pause
     exit %EXITCODE%
 )
 
+echo Установка JavaFX...
+python -u .\build\install_jfx.py
+set /a EXITCODE = %ERRORLEVEL%
+
+if %EXITCODE% neq 0 (
+    echo Ошибка при установке JavaFX...
+    pause
+    exit %EXITCODE%
+)
+
 echo Запуск build.py для сборки проекта...
 python -u .\build\build.py
-
 set /a EXITCODE = %ERRORLEVEL%
+
 if %EXITCODE% neq 0 (
     echo Ошибка при сборке проекта...
     pause
