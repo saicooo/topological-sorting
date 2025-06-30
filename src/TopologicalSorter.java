bab039d3e77222b76f094f8d7e51e562278debf4
Stepan Ivanov <stepanviolet@gmail.com>
2025-06-30 17:51:30 +0300

    TopSort: fix bug in prev & add mutable getter for graph

--- src/TopologicalSorter.java
+++ src/TopologicalSorter.java
@@ -6,12 +6,14 @@ public class TopologicalSorter implements ExtendedIterator<Vertex> {
     private final Map<Vertex, Integer> inDegree;
     private final Deque<Vertex> queue;
     private final List<Vertex> result;
+    private final Deque<List<Vertex>> addedToQueueHistory;
 
     public TopologicalSorter(Graph graph) {
         this.graph = graph;
         this.inDegree = new HashMap<>();
         this.queue = new ArrayDeque<>();
         this.result = new ArrayList<>();
+        this.addedToQueueHistory = new ArrayDeque<>();
 
         for (Vertex v : graph.getVertices()) {
             inDegree.put(v, 0);
@@ -31,13 +33,17 @@ public class TopologicalSorter implements ExtendedIterator<Vertex> {
 
     }
 
+    public Graph getGraph() { // TODO: return a copy or an unmodifiable view
+        return graph;
+    }
+
     @Override
     public boolean hasNext() {
         return !queue.isEmpty();
     }
 
     @Override
-    public Vertex next() {
+    public Vertex next() throws NoSuchElementException {
         if (!hasNext()) {
             throw new NoSuchElementException("No more elements");
         }
@@ -49,36 +55,42 @@ public class TopologicalSorter implements ExtendedIterator<Vertex> {
         Vertex v = queue.removeFirst();
         result.add(v);
 
+        List<Vertex> addedNow = new ArrayList<>();
         for (Vertex neigh : v.getNeighbors()) {
             int deg = inDegree.get(neigh) - 1;
             inDegree.put(neigh, deg);
             if (deg == 0) {
                 queue.addLast(neigh);
+                addedNow.add(neigh);
             }
         }
+        addedToQueueHistory.push(addedNow);
         return v;
     }
 
     @Override
-    public Vertex prev() {
+    public Vertex prev() throws NoSuchElementException {
         if (result.isEmpty()) {
             throw new NoSuchElementException("No previous element to revert");
         }
+        
         Vertex v = result.remove(result.size() - 1);
+        List<Vertex> addedNow = addedToQueueHistory.pop();
         for (Vertex neighbor : v.getNeighbors()) {
-            if (inDegree.get(neighbor) == 0) {
-                queue.remove(neighbor);
-            }
             inDegree.put(neighbor, inDegree.get(neighbor) + 1);
         }
+
+        for (Vertex neighbor : addedNow) {
+            queue.remove(neighbor);
+        }
+
         queue.addFirst(v);
         return v;
     }
 
-
     public List<Vertex> getSortedSoFar() {
         return Collections.unmodifiableList(result);
     }
-
     
 }
+
