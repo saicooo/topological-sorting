0f29f31c6f46baee956bd7068969aed6c3c1f25e
Stepan Ivanov <stepanviolet@gmail.com>
2025-07-02 17:25:57 +0300

    start_app: fix start command

--- build/start.py
+++ build/start.py
@@ -34,8 +34,14 @@ def start(main_class: str | None = None):
     log.info(f"Запуск приложения с главным классом: {main_class}")
     
     classpath = get_classpath(OUT_DIR, LIB_DIR)
-    command = f"java --module-path {LIB_DIR / f'javafx-sdk-{JFX_VERSION}' / 'lib'} --add-modules javafx.controls,javafx.fxml -cp {classpath} {main_class}"
-    
+    command = [
+        "java", 
+        "--module-path", LIB_DIR / f'javafx-sdk-{JFX_VERSION}' / 'lib', 
+        "--add-modules", "javafx.controls,javafx.fxml", 
+        "-cp", classpath,
+         main_class
+    ]
+
     log.debug(f"Команда для запуска: {command}")
     result = subprocess.run(command, text=True, capture_output=True)
     
