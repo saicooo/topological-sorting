9efccb43f7e50db0aa4e43f2171522ed6237cbe0
Stepan Ivanov <stepanviolet@gmail.com>
2025-07-02 16:19:07 +0300

    start_app: add launch script 4 windows

--- start.bat
+++ start.bat
@@ -1,20 +1,3 @@
 @echo off
-chcp 65001 >nul
-
-REM Скрипт для запуска проекта на Windows
-
-if "%~1"=="" (
-    echo Использование: %~nx0 MainClass
-    exit /b 1
-)
-
-set MAIN_CLASS=%1
-
-python -u .\build\start.py %MAIN_CLASS%
-set /a EXITCODE=%ERRORLEVEL%
-
-if %EXITCODE% neq 0 (
-    echo Ошибка при запуске проекта...
-    exit /b %EXITCODE%
-)
+start cmd /k ".\build\start_app.bat"
 
